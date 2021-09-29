78517cf75c4a6021b8825e8d4b5ac2d935b00eb2
clementcreusat <clement.creusat@opendigitaleducation.com>
2021-09-29 09:19:18 +0200

    fixes

--- assets/js/search.js
+++ assets/js/search.js
@@ -22,32 +22,32 @@ export const Search = () => {
     <div class="user__top">
       <figure class="user__avatar"><img src=${user.avatar_url} width="120" height="120" alt=${user.login}></figure>
       <div>
-        <strong class="user__name">${user.name ? user.name : user.login}</strong>
+        <h2 class="user__name">${user.name ? user.name : user.login}</h2>
         <span class="user__login">@${user.login}</span>
         <p class="user__date">Joined ${date}</p>
       </div>
     </div>
     <div class="user__content">
       <p class="user__text text">${user.bio ? user.bio : "This profile has no bio"}</p>
-      <div class="user__stats">
-        <div><span class="text">Repos</span> <strong>${user.public_repos}</strong></div>
-        <div><span class="text">Followers</span> <strong>${user.followers}</strong></div>
-        <div><span class="text">Following</span> <strong>${user.following}</strong></div>
-      </div>
+      <ul class="user__stats">
+        <li><span class="text">Repos</span> <strong>${user.public_repos}</strong></li>
+        <li><span class="text">Followers</span> <strong>${user.followers}</strong></li>
+        <li><span class="text">Following</span> <strong>${user.following}</strong></li>
+      </ul>
       <ul class="user__info">
         <li class="user__item text">
-        <div class="image-container"><img src="/assets/images/icon-location.svg" height="20" alt="location" /></div> ${user.location
+        <div class="image-container"><img src="/assets/images/icon-location.svg" height="20" alt="" aria-hidden="true" /></div> ${user.location
             ? user.location
             : "<span class='text-disabled'>Not Available</span>"}</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-twitter.svg" height="20" alt="twitter" /></div> ${user.twitter_username
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-twitter.svg" height="20" alt="" aria-hidden="true" /></div> ${user.twitter_username
             ? user.twitter_username
             : "<span class='text-disabled'>Not Available</span>"}</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-website.svg" height="20" alt="website" /></div> ${user.blog
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-website.svg" height="20" alt="" aria-hidden="true" /></div> ${user.blog
             ? "<a target='_blank' rel='noopener' href='" +
                 user.blog +
                 "'>Website link</a>"
             : "<span class='text-disabled'>Not Available</span>"}</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-company.svg" height="20" alt="company" /></div> ${user.company
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-company.svg" height="20" alt="" aria-hidden="true" /></div> ${user.company
             ? user.company
             : "<span class='text-disabled'>Not Available</span>"}</li>
       </ul>
--- assets/js/search.ts
+++ assets/js/search.ts
@@ -17,9 +17,9 @@ export const Search = () => {
         user.avatar_url
       } width="120" height="120" alt=${user.login}></figure>
       <div>
-        <strong class="user__name">${
+        <h2 class="user__name">${
           user.name ? user.name : user.login
-        }</strong>
+        }</h2>
         <span class="user__login">@${user.login}</span>
         <p class="user__date">Joined ${date}</p>
       </div>
@@ -28,37 +28,37 @@ export const Search = () => {
       <p class="user__text text">${
         user.bio ? user.bio : "This profile has no bio"
       }</p>
-      <div class="user__stats">
-        <div><span class="text">Repos</span> <strong>${
+      <ul class="user__stats">
+        <li><span class="text">Repos</span> <strong>${
           user.public_repos
-        }</strong></div>
-        <div><span class="text">Followers</span> <strong>${
+        }</strong></li>
+        <li><span class="text">Followers</span> <strong>${
           user.followers
-        }</strong></div>
-        <div><span class="text">Following</span> <strong>${
+        }</strong></li>
+        <li><span class="text">Following</span> <strong>${
           user.following
-        }</strong></div>
-      </div>
+        }</strong></li>
+      </ul>
       <ul class="user__info">
         <li class="user__item text">
-        <div class="image-container"><img src="/assets/images/icon-location.svg" height="20" alt="location" /></div> ${
+        <div class="image-container"><img src="/assets/images/icon-location.svg" height="20" alt="" aria-hidden="true" /></div> ${
           user.location
             ? user.location
             : "<span class='text-disabled'>Not Available</span>"
         }</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-twitter.svg" height="20" alt="twitter" /></div> ${
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-twitter.svg" height="20" alt="" aria-hidden="true" /></div> ${
           user.twitter_username
             ? user.twitter_username
             : "<span class='text-disabled'>Not Available</span>"
         }</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-website.svg" height="20" alt="website" /></div> ${
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-website.svg" height="20" alt="" aria-hidden="true" /></div> ${
           user.blog
             ? "<a target='_blank' rel='noopener' href='" +
               user.blog +
               "'>Website link</a>"
             : "<span class='text-disabled'>Not Available</span>"
         }</li>
-        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-company.svg" height="20" alt="company" /></div> ${
+        <li class="user__item text"><div class="image-container"><img src="/assets/images/icon-company.svg" height="20" alt="" aria-hidden="true" /></div> ${
           user.company
             ? user.company
             : "<span class='text-disabled'>Not Available</span>"
